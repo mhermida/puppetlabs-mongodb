c0f8ef65f64bc779bc637794dbe3c5b8fe36cfd4
Marcos Hermida Mera <marcos.mera@ecmwf.int>
2017-05-25 12:11:31 +0100

    Support for adding roles defined with hashes containing keys db and role

--- lib/puppet/type/mongodb_user.rb
+++ lib/puppet/type/mongodb_user.rb
@@ -6,7 +6,9 @@ Puppet::Type.newtype(:mongodb_user) do
   def initialize(*args)
     super
     # Sort roles array before comparison.
-    self[:roles] = Array(self[:roles]).sort!
+    # Munged values safe to sort by db and role
+    Puppet.debug(self[:roles])
+    self[:roles] = Array(self[:roles]).sort! { |a, b| [a['db'], a['role' ]] <=> [b['db'], b['role']] }
   end
 
   newparam(:name, :namevar=>true) do
@@ -38,7 +40,35 @@ Puppet::Type.newtype(:mongodb_user) do
   newproperty(:roles, :array_matching => :all) do
     desc "The user's roles."
     defaultto ['dbAdmin']
-    newvalue(/^\w+$/)
+    # newvalue(/^\w+$/)
+   
+    validate do |value|
+      klass = value.class
+      val_regex = /^\w+$/
+      case klass.to_s
+      when "String"
+        return value if value.match(val_regex)
+        raise ArgumentError,
+          "Roles defined with Strings must be single words"
+      when "Hash"
+        return value if ((value.key?('db') and value.key?('role')) \
+                         and (value['db'].match(val_regex) and value['role'].match(val_regex))) 
+        raise ArgumentError,
+          "roles defined with hashes must contain valid values for keys 'db' and 'role'"
+      else
+        raise ArgumentError,
+          "Roles must be String or Hash"
+      end	
+    end 
+
+    munge do |value|
+      if value.class == String
+        Puppet.debug("We gotta munge this #{value}")
+        { "role" => value, "db" => @resource[:database] }
+      else
+        value
+      end
+    end	
 
     # Pretty output for arrays.
     def should_to_s(value)
